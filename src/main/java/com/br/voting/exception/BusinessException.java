259ebe018d4a6701cad25f9c2f36d3f9001af99e
Alison8547 <alisonailson100@hotmail.com>
2024-05-30 00:02:41 -0300

    feat: add exception handler

---
+++ src/main/java/com/br/voting/exception/BusinessException.java
@@ -0,0 +1,12 @@
+package com.br.voting.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+public class BusinessException extends RuntimeException{
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
